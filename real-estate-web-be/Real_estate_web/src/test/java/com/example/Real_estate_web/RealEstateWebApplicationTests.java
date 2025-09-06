146b0bea58171af8c251c7ee26954b009f66a4a5
meouisme <118751514+meouisme@users.noreply.github.com>
2025-09-06 20:47:49 +0700

    create web

---
+++ real-estate-web-be/Real_estate_web/src/test/java/com/example/Real_estate_web/RealEstateWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Real_estate_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
